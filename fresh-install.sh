2217893415be50074114c8890a6c286c86971c5e
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-31 17:36:09 +0530

    [WIP] Read packages from file

--- fresh-install.sh
+++ fresh-install.sh
@@ -1,24 +1,58 @@
 #!/bin/bash
 
-echo -n "Enter the name of your package manager [APT,YUM,PACMAN] "
-read MANAGER
+PACKAGES=()
 
-case $MANAGER in
 
-APT)
-    __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-    bash ${__dir}/apt.sh
-    ;;
+install_flutter() {
+    sudo apt install -y vlc
+}
 
-YUM)
-    echo -n "yum"
-    ;;
+user_prompt() {
+    read -t 1 -n 10000 discard
+    read -p "Do you want to install $1 (y/n)? " answer
+    case ${answer:0:1} in
+    y | Y)
+        PACKAGES+=("$1")
+        ;;
+    *)
+        echo No
+        ;;
+    esac
+}
 
-PACMAN)
-    echo -n "pacman"
-    ;;
+get_package_manager() {
 
-*)
-    echo -n "unknown"
-    ;;
-esac
+    echo -n "Enter the name of your package manager [APT,YUM,PACMAN] "
+    read MANAGER
+
+    case $MANAGER in
+
+    APT)
+        __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+        bash ${__dir}/apt.sh
+        ;;
+
+    YUM)
+        echo -n "yum"
+        ;;
+
+    PACMAN)
+        echo -n "pacman"
+        ;;
+
+    *)
+        echo -n "unknown"
+        ;;
+    esac
+}
+
+filename='common.txt'
+while read line; do
+    # reading each line
+    echo "Line : $line"
+    user_prompt $line
+    
+done <$filename
+
+
+echo "Packages are $PACKAGES."
