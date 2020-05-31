840372af91d9cb1543ed597ade6efdfb56288c0c
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-31 17:01:54 +0530

    fix: user prompt

--- fresh-install.sh
+++ fresh-install.sh
@@ -2,15 +2,19 @@
 
 PACKAGES=()
 
+install_flutter () {
+    echo "Installing flutter"
+}
+
 user_prompt () {
  read -p "Do you want to install $1 (y/n)? " answer
 case ${answer:0:1} in
     y|Y )
-        echo $2
+        $2
     ;;
     * )
         echo No
     ;;
 esac
 }
-user_prompt Flutter installing
+user_prompt Flutter install_flutter
