8661312c7a3d153a07b9d56d15e97dbc1fd250a4
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-09-05 15:27:17 +0530

    feat: docker-compose install

--- setup.sh
+++ setup.sh
@@ -28,6 +28,12 @@ sudo add-apt-repository \
 sudo apt update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
+
+#### DOCKER COMPOSE SETUP
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
 ### NODE-JS SETUP ####
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 source ~/.bashrc
