ab0af908dd3de51bc26602b3a4fe592165136eb0
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-09-18 19:04:06 +0530

    feat: arch setup

---
+++ setup_arch.sh
@@ -0,0 +1,10 @@
+sudo pacman -Syyu
+yay -Syyu
+
+
+git clone https://github.com/theProgrammerDavid/config-files ~/config-files
+cd ~/config-files
+bash ./setup.sh
+
+sudo pacman -S docker docker-compose openssh p7zip p7zip-plugins unrar tar rsync 
+ yay -S nodejs
